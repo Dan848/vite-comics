694d326b6e1f1a3130e72227e85b76ce6aa853e1
Dan848 <daniele.ceravolo15@gmail.com>
2023-04-14 07:05:24 +0200

    first commit data created upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Data Loaded
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/data/data.js
@@ -0,0 +1,188 @@
+const headerLinks = [
+    {
+        text: "characters",
+        active: false
+    },
+    {
+        text: "comics",
+        active: true
+    },
+    {
+        text: "movies",
+        active: false
+    },
+    {
+        text: "tv",
+        active: false
+    },
+    {
+        text: "games",
+        active: false
+    },
+    {
+        text: "collectibles",
+        active: false
+    },
+    {
+        text: "videos",
+        active: false
+    },
+    {
+        text: "fans",
+        active: false
+    },
+    {
+        text: "news",
+        active: false
+    },
+    {
+        text: "shop",
+        active: false
+    }
+]
+
+const bridgeSection = [
+    {
+        src: "buy-comics-digital-comics.png",
+        text: "digital comics"
+    },
+    {
+        src: "buy-comics-merchandise.png",
+        text: "dc merchandise"
+    },
+    {
+        src: "buy-comics-subscriptions.png",
+        text: "subscription"
+    },
+    {
+        src: "buy-comics-shop-locator.png",
+        text: "comic shop locator"
+    },
+    {
+        src: "buy-dc-power-visa.svg",
+        text: "dc power visa"
+    }
+]
+
+const footerLinks = [
+    {
+        title: "dc comics",
+        links: [
+            {
+                text: "characters",
+                href: "#"
+            },
+            {
+                text: "comics",
+                href: "#"
+            },
+            {
+                text: "movies",
+                href: "#"
+            },
+            {
+                text: "tv",
+                href: "#"
+            },
+            {
+                text: "games",
+                href: "#"
+            },
+            {
+                text: "videos",
+                href: "#"
+            },
+            {
+                text: "news",
+                href: "#"
+            },
+        ]
+    },
+    {
+        title: "shop",
+        links: [
+            {
+                text: "shop DC",
+                href: "#"
+            },
+            {
+                text: "shop DC collectibles",
+                href: "#"
+            }
+        ]
+    },
+    {
+        title: "dc",
+        links: [
+            {
+                text: "terms of use",
+                href: "#"
+            },
+            {
+                text: "privacy poolicy (new)",
+                href: "#"
+            },
+            {
+                text: "ad choices",
+                href: "#"
+            },
+            {
+                text: "advertising",
+                href: "#"
+            },
+            {
+                text: "jobs",
+                href: "#"
+            },
+            {
+                text: "subscriptions",
+                href: "#"
+            },
+            {
+                text: "talent workshops",
+                href: "#"
+            },
+            {
+                text: "CPSC certificates",
+                href: "#"
+            },
+            {
+                text: "ratings",
+                href: "#"
+            },
+            {
+                text: "show help",
+                href: "#"
+            },
+            {
+                text: "contact us",
+                href: "#"
+            },
+        ]
+    },
+    {
+        title: "sites",
+        links: [
+            {
+                text: "DC",
+                href: "#"
+            },
+            {
+                text: "MAD magazine",
+                href: "#"
+            },
+            {
+                text: "DC kids",
+                href: "#"
+            },
+            {
+                text: "DC universe",
+                href: "#"
+            },
+            {
+                text: "DC power visa",
+                href: "#"
+            }
+        ]
+    },
+]
